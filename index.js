b680f2bb596d48dbb5a93ff876553e9d0c97843d
Franklin Benjamin <franciscovibes14@gmail.com>
2022-06-03 23:07:21 -0400

    commit tree 6e22ebd38e6de1d729bf7d45d45a14aec1d8e679

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const obj = record.find(function (obj) {
+        return obj.result === "W"
+    })
+
+    if (obj) {
+        return obj.year
+    }
+}
